8dedde2214460a3892c4689e84d22931734a0589
RaviJangir <ravijangir993@gmail.com>
2024-07-03 19:28:16 +0530

    commit

--- week 2/array01.cpp
+++ week 2/array01.cpp
@@ -1,7 +1,30 @@
 #include<iostream>
 #include<string.h>
+#include <limits.h>
 
 using namespace std;
+
+void printArr(int arr[], int size) {
+    for(int i=0; i<size; i++){
+        cout<<arr[i]<< " ";
+    }
+    cout<<endl;
+}
+
+void Inc(int arr[], int size) {
+    arr[0] +=10;
+    printArr(arr, size);
+}
+
+bool find(int arr[], int size, int key) {
+    for(int i=0; i<size; i++) {
+        if(arr[i] == key) return true;
+    }
+    return false;
+}
+
+
+
 
 int main() {
     // int arr[] = {1,2,3,4,5};
@@ -37,17 +60,104 @@ int main() {
 
 
     // Q2
-    int arr[5]= {1,3,5,7,9};
+    // int arr[5]= {1,3,5,7,9};
+    // // for(int i=0; i<5; i++) {
+    // //     arr[i] = 1;
+    // // }
+
+    // // memset function
+    // memset(arr, 1, sizeof(arr));
+    // cout<<"Printing the values in array "<<endl;
     // for(int i=0; i<5; i++) {
-    //     arr[i] = 1;
+    //     cout<< arr[i] << " ";
     // }
 
-    // memset function
-    memset(arr, 1, sizeof(arr));
-    cout<<"Printing the values in array "<<endl;
-    for(int i=0; i<5; i++) {
-        cout<< arr[i] << " ";
+
+    // int arr[] = {5,6};
+    // int size=2;
+    // Inc(arr, size);
+    // printArr(arr, size);
+
+
+    // find key in arr using linear search
+    // int arr[5] = {6,2,3,4,8};
+    // int size = 5;
+    // int key;
+    // cout<<"Enter the key to find in array"<<endl;
+    // cin>>key;
+    // if(find(arr, size, key)){
+    //     cout<<"Found";
+    // }
+    // else{
+    //     cout<<"Not found";
+    // }
+
+
+    // count 0's and 1's in array
+    // 0
+
+
+    // find maximum in arr
+    // int arr[] = {2,-3,5,1,40,6};
+    // int size = 6;
+    // int maxNum = INT_MIN;
+    // for(int i=0; i<size; i++) {
+    //     if(arr[i] > maxNum)  {
+    //         maxNum = arr[i];
+    //     }
+    // } 
+    // cout<<"Maximus is = "<<maxNum;
+
+
+    // // Find minimaum in arr
+    // int minNum = INT_MAX;
+    // for(int i=0; i< size; i++) {
+    //     if(arr[i] < minNum) {
+    //         minNum = arr[i];
+    //     }
+    // }
+    // cout<<"Minimum number is = "<<minNum;
+
+
+    // Print extereme values
+    // int arr[7] = {10,20,30,40,50,60,70};
+    // int size = 7;
+    // int start = 0;
+    // int end = size-1;
+    // while(start <= end) {
+    //     if(start == end) {
+    //         cout<<arr[start]<< " ";
+    //     }
+    //     else {
+    //         cout<<arr[start]<< " ";
+    //         cout<<arr[end]<<" ";
+    //     }
+    //     start++;
+    //     end--;
+    // }
+
+
+    // Reverse an array
+    int arr[7] = {10,20,30,40,50,60,70};
+    int size = 7;
+    // int start = size-1;
+    // while(start>=0) {
+    //     cout<<arr[start]<<" ";
+    //     start--;
+    // }
+
+    // by swap
+    int s =0;
+    int e = size-1;
+    while(s<=e) {
+        swap(arr[s], arr[e]);
+        s++;
+        e--;
     }
+    for(int i=0; i<size; i++) {
+        cout<<arr[i]<< " ";
+    }
+
 
     return 0;
 }
