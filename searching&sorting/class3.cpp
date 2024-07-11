91d1685154e61a74812c920c07900527fb896fe5
RaviJangir <ravijangir993@gmail.com>
2024-07-11 23:17:53 +0530

    commit

---
+++ searching&sorting/class3.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int findTargetInNearly(vector<int> a, int target){
+    int s = 0;
+    int e = a.size() -1;
+
+    while (s <= e)
+    {
+        int mid = s + (e-s)/2;
+        if(target == a[mid]){
+            return mid;
+        }
+        if(mid-1 >= s && target == a[mid-1]) {
+            return mid-1;
+        }
+        if(mid+1 <= e && target == a[mid+1]) {
+            return mid +1;
+        }
+        if(target < a[mid]) {
+            e = mid-2;
+        }
+        else{
+            s = mid+2;
+        }
+    }
+    return -1;
+}
+
+
+int findQuotient(int dividend, int divisor){
+    int s=0;
+    int e=abs(dividend);
+    int ans = 0;
+
+    while(s <= e) {
+        int mid = s + (e-s)/2;
+        if(abs(mid * divisor) == abs(dividend)) {
+            ans = mid;
+            break;
+        }
+        if(abs(mid * divisor) < abs(dividend)) {
+            ans = mid;
+            s = mid+1;
+        }
+        else{
+            e = mid-1;
+        }
+    }
+    if((divisor<0 && dividend<0) || (divisor>0 && dividend>0)) 
+        return ans;
+    else {
+        return -ans;
+    }
+}
+
+int main() {
+    // FINDING ELEMENT IN NEARLY SORTED ARRAY
+    // vector<int> a{10,3,40,20,50,80,70};
+    // int target;
+    // cin>>target;
+
+    // int ans = findTargetInNearly(a, target);
+    // cout<<"Index of "<<target<<" is "<<ans<<endl;
+
+
+    // DIVISION USING BS
+    int dividend,divisor, precision;
+    cin>>dividend;
+    cin>>divisor;
+    int ans = findQuotient(dividend, divisor);
+
+    cout<<"Quotient is: "<<ans<<endl;
+    cout<<"Enter the precison "<<endl;
+    cin>>precision;
+
+    double finalAns = ans;
+    double step = 0.1;
+    for(int i=0; i<precision; i++) {
+        for(double j=finalAns; j*divisor <= dividend; j+=step) {
+            finalAns = j;
+        }
+        step = step/10;
+    }
+    cout<<"Ans after precision is : "<<finalAns<<endl;
+
+    return 0;
+}
