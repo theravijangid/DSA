e92bd59c7c3257abdbc27437183a3a6dfe834433
RaviJangir <ravijangir993@gmail.com>
2024-07-13 23:14:21 +0530

    commit

--- searching&sorting/sorting.cpp
+++ searching&sorting/sorting.cpp
@@ -21,20 +21,47 @@ int main() {
 
 
     // BUBBLE SORT
+    // vector<int> a{10,1,7,6,14,9};
+    // int n=a.size();
+
+    // for(int round =1; round<n; round++) {
+    //     bool isSwapped = false;
+    //     for(int j=0; j< n-round; j++) {
+    //         if(a[j] > a[j+1]){
+    //             isSwapped = true;
+    //             swap(a[j], a[j+1]);
+    //         }
+    //     }
+    //     if(!isSwapped) {
+    //         break;
+    //     }
+    // }
+
+    // for(int i=0; i<n; i++) {
+    //     cout<<a[i]<<" ";    
+    // }
+
+
+    // INSERTION SORT
     vector<int> a{10,1,7,6,14,9};
     int n=a.size();
 
-    for(int round =1; round<n; round++) {
-        bool isSwapped = false;
-        for(int j=0; j< n-round; j++) {
-            if(a[j] > a[j+1]){
-                isSwapped = true;
-                swap(a[j], a[j+1]);
+    for(int i=1; i<n; i++) {
+        // step A : fetch i
+        int val = a[i];
+        // step B : compare
+        int j;
+        for(j=i-1; j>=0; j--) {
+            if(a[j] > val) {
+                // step C : Shift
+                a[j+1] = a[j]; 
+            }
+            else {
+                break;
             }
         }
-        if(!isSwapped) {
-            break;
-        }
+        // step D: copy i
+        a[j+1] = val;
     }
 
     for(int i=0; i<n; i++) {
