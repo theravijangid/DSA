f6699d6e1dea13140d086767103f2dc244ef13f1
RaviJangir <ravijangir993@gmail.com>
2024-07-20 19:19:34 +0530

    commit

--- week 2/hw.cpp
+++ week 2/hw.cpp
@@ -416,3 +416,84 @@ int main() {
     return 0;
     
 }
+
+
+
+// DEBUGGING
+// 1
+// #include <iostream>
+// using namespace std;
+
+// int binarySearch(int arr[], int left, int right, int x) {
+//     if (right >= left) {
+//         int mid = (left + right) / 2;
+//         if (arr[mid] == x)
+//             return mid;
+//         else if (arr[mid] > x)
+//             return binarySearch(arr, left, mid - 1, x);
+//         else
+//             return binarySearch(arr, mid + 1, right, x);
+//     }
+//     return -1;
+// }
+
+// int main() {
+//     int arr[] = { 2, 3, 4, 10, 40 };
+//     int n = sizeof(arr) / sizeof(arr[0]);
+//     int x = 10;
+//     int result = binarySearch(arr, 0, n - 1, x);
+//     if (result == -1)
+//         cout << "Element not found.";
+//     else
+//         cout << "Element found at index " << result << endl;
+//     return 0;
+// }
+
+//2 
+// int ceilIndex(int arr[], int n, int target) {
+//   int left = 0;
+//   int right = n - 1;
+//   int ceil = -1;
+
+//   while (left <= right) {
+//     int mid = left + (right - left) / 2;
+//     if (arr[mid] == target)
+//       return mid;
+//     else if (arr[mid] < target) {
+//       left = mid + 1;
+//     } else {
+//       ceil = mid;
+//       right = mid - 1;
+//     }
+//   }
+
+//   return ceil;
+// }
+
+
+// 3
+// for(int i=0; i<n; i++) {
+    //     // find minimum
+    //     int minIndex = i;
+    //     for(int j=i+1; j<n; j++) {
+    //         if(a[j] < a[minIndex]) {
+    //             minIndex=j;
+    //         }
+    //     }
+    //     //swap
+    //     swap(a[i], a[minIndex]);
+    // }
+
+// 4
+// for(int round =1; round<n; round++) {
+//     bool isSwapped = false;
+//     for(int j=0; j< n-round; j++) {
+//         if(a[j] > a[j+1]){
+//             isSwapped = true;
+//             swap(a[j], a[j+1]);
+//         }
+//     }
+//     if(!isSwapped) {
+//         break;
+//     }
+// }
