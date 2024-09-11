08b455d387e0c58bfead49dd6057ba5d3c01ac10
RaviJangir <ravijangir993@gmail.com>
2024-09-12 00:04:47 +0530

    commit

--- DnC/N_Queen.cpp
+++ DnC/N_Queen.cpp
@@ -81,3 +81,66 @@ int main() {
     
     return 0;
 }
+
+// LEETCODE
+// class Solution {
+// public:
+//     unordered_map<int, bool> rowCheck;
+//     unordered_map<int,bool> upperLeftDiagnolCheck;
+//     unordered_map<int,bool> lowerLeftDiagnolCheck;
+
+//     void storeSolution(vector<vector<char>> &board, int n, vector<vector<string> > &ans) {
+//         vector<string> temp;
+//         for(int i = 0; i < n; i++) {
+//             string output = "";
+//             for(int j = 0; j < n; j++) {
+//                 output.push_back(board[i][j]);
+//             }
+//             temp.push_back(output);
+//         }
+//         ans.push_back(temp);
+//     }
+
+//     bool isSafe(vector<vector<char>> &board, int row, int col, int n) {
+//         if(rowCheck[row] == true) return false;
+
+//         if(upperLeftDiagnolCheck[n-1+col-row] == true) return false;
+
+//         if(lowerLeftDiagnolCheck[col+row] == true) return false;
+
+//         return true;
+//     }
+
+//     void solve(vector<vector<char>> &board, int col, int n, vector<vector<string>> &ans ) {
+//         if(col >= n) {
+//             storeSolution(board, n, ans);
+//             return;
+//         }
+
+//         //1 case solve karna h , baaki recursion sambhal lega
+//         for(int row= 0; row < n; row++) {
+//             if(isSafe(board, row, col, n)) {
+//                 board[row][col] = 'Q';
+//                 rowCheck[row] = true;
+//                 upperLeftDiagnolCheck[n-1+col-row] = true;
+//                 lowerLeftDiagnolCheck[col+row] = true;
+
+//                 // RR
+//                 solve(board, col + 1, n, ans);
+                
+//                 // backtrack
+//                 board[row][col] = '.';
+//                 rowCheck[row] = false;
+//                 upperLeftDiagnolCheck[n-1+col-row] = false;
+//                 lowerLeftDiagnolCheck[col+row] = false;
+//             }   
+//         }
+//     }
+
+//     vector<vector<string>> solveNQueens(int n) {
+//         vector<vector<char>> board(n, vector<char>(n,'.'));
+//         vector<vector<string>> ans;
+//         solve(board, 0, n, ans);
+//         return ans;
+//     }
+// };
